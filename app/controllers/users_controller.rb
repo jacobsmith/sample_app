fa1510c9677b614a68b28a53c2520389f6bfe30c
Matthew Newell <matthewnewell@unknown002436b3c039.gateway.pace.com>
2013-08-06 06:40:32 -0400

    Completed sign in / sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to Thank the Teacher!"
       redirect_to @user 
     else
