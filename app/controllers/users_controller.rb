65c8b07eecf375843a2f734ec0c9861dba7cc180
Matthew Newell <matthewnewell@unknown002436b3c039.gateway.pace.com>
2013-08-06 22:59:02 -0400

    in progress checkin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+     sign_in @user
+     redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
