2205ea03cc6076426166a4e34bb8fd91674c938f
Matthew Newell <matthewnewell@unknown002436b3c039.gateway.pace.com>
2013-08-07 21:32:15 -0400

    in progress commit

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,7 +36,42 @@ describe "Authentication" do
     
       describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link('Sign in) }
+        it { should have_link('Sign in') }
+      end
+    end
+  end
+
+  describe "autorization" do
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+        
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title('Edit user')) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
   end
